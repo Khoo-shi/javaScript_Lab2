11b0ba28f862ffb9ff3b7d0e71a269b03b171ea7
khoo_shi <Khushdeepk1706@gmail.com>
2025-10-16 21:57:10 -0400

    Added function to change background color using RGBvalues

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -9,3 +9,20 @@ const pageBody = document.body;
 redSlider.addEventListener('input', changeBackgroundColor);
 greenSlider.addEventListener('input', changeBackgroundColor);
 blueSlider.addEventListener('input', changeBackgroundColor);
+
+//  change the background color of the page
+function changeBackgroundColor() {
+  // Get the values of each color
+  const red = redSlider.value;
+  const green = greenSlider.value;
+  const blue = blueSlider.value;
+
+  
+  const rgbColor = `rgb(${red}, ${green}, ${blue})`;
+
+  // Applies the new background color to the page
+  pageBody.style.backgroundColor = rgbColor;
+
+  // Log to console to test
+  console.log('Color changed to:', rgbColor);
+}
